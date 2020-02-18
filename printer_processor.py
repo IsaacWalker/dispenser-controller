8ae6cce2186c351c7accc0f77a56cccf85dc704f
IsaacWalker <isaacwalker1377@gmail.com>
2020-02-18 17:36:21 +0000

    Changed JobId to UUID

--- printer_processor.py
+++ printer_processor.py
@@ -1,6 +1,31 @@
+import uuid 
+import printer_pb2
 
-def print_medication(request):
-    print('Request Received from ' + request.first_name + " "+ request.last_name)
+# Job Id (UUID) => job
+job_dict = dict()
 
-def check_health(request):
-    print('Healthy!')
+# A print job has an Id, a status and a path to the stl,
+class printJob:
+    def __init__(self, expected_time):
+        self.id = uuid.uuid4()
+        self.expected_time = expected_time
+        self.status = printer_pb2.GetJobStatusResponse.JobStatus.NOT_STARTED
+
+def create_print_job(request):
+    # Creates a new job and adds it to the dictionary
+    job = printJob(100)
+    job_dict[job.id] = job
+
+    # Returns the job
+    return job
+
+def run_print_job(request):
+    # Runs the print job 
+
+    # Updates the Print Job status
+    job_dict[request.id].status = printer_pb2.GetJobStatusResponse.JobStatus.STARTED
+
+def get_job_status(request):
+    # Gets the jobs status
+    return job_dict[request.job_id]
+
