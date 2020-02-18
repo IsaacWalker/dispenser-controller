830d65362703fe8a9923b32e11a62516b65ebc96
Dominick Cleary <docleary@tcd.ie>
2020-02-18 05:35:39 +0000

    Added PrusaSlicer and gcode generation

--- odf.py
+++ odf.py
@@ -1,6 +1,6 @@
 import openpyscad as op
 import printer_pb2
-from os import listdir
+import os
 from subprocess import call
 
 def generate_odf_scad(unique_file_name, odf):
@@ -13,10 +13,13 @@ def generate_odf_scad(unique_file_name, odf):
 odf = printer_pb2.ODF(label="\"Label\"",drug_name='Drug',density=1.25,length= 25.0, width=30,height=1.5)
 generate_odf_scad('test.scad', odf)
 
-files = listdir(".")
+files = os.listdir(".")
 
 for file in files:
     if file.find(".scad") >= 0:
         of = file.replace(".scad", ".stl")
         cmd = 'call (["openscad",  "-o", "{}",  "{}"])'.format(of, file)   #create openscad command        
         exec(cmd)
+
+generate_gcode = "./PrusaSlicer/PrusaSlicer-2.1.1.AppImage --g test.stl --load prusai3mk3.ini --output test.gcode"
+os.system(generate_gcode)
