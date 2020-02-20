40d4894f756b9c3958d38648cbb2f3477f246bd6
IsaacWalker <isaacwalker1377@gmail.com>
2020-02-20 20:58:46 +0000

    Added test scad generation

--- odf.py
+++ odf.py
@@ -10,22 +10,23 @@ length = math.sqrt(0.8 * dosage)
 
 def generate_odf_scad(unique_file_name, odf):
    
-    odf_text =  op.Text('\"'+odf.label+'\"', 4).linear_extrude(0.1).translate([5,-14,1.6]).rotate([0,0,90]).color('Red')
+    odf_name_text =  op.Text('\"'+odf.label+'\"', 4).linear_extrude(0.3).translate([5,-10,1.5]).rotate([0,0,90]).color('Red')
+    odf_dosage_text =  op.Text('\"'+"{:.2f} mg".format(odf.density)+'\"', 4).linear_extrude(0.3).translate([5,-18,1.5]).rotate([0,0,90]).color('White')
     odf_cube= op.Cube([odf.length, odf.width, odf.height])
-    odf_scad = (odf_text + odf_cube) 
-    odf_scad.write('scads/'+unique_file_name)
+    odf_scad = (odf_name_text + odf_dosage_text + odf_cube) 
+    odf_scad.write(unique_file_name)
 
-#print ("Generating SCAD")
-#odf = printer_pb2.ODF(label="\"Label\"",drug_name='Drug',density=1.25,length= length, width=length,height=1.5)
-#generate_odf_scad('test.scad', odf)
+print ("Generating SCAD")
+odf = printer_pb2.ODF(label='T. Davis',drug_name='Drug',density=0.84,length= 25, width=30,height=1.5)
+generate_odf_scad('test.scad', odf)
 
-#print ("Generating STL from SCAD")
-#generate_stl = "openscad -o test.stl test.scad"
-#os.system(generate_stl)
+print ("Generating STL from SCAD")
+generate_stl = "openscad -o test.stl test.scad"
+os.system(generate_stl)
 
-#print ("Generating GCODE from STL")
-#generate_gcode = "prusa-slicer-console -g test.stl --load printer_profiles/" + profile_name + " -o gcode/test.gcode"
-#os.system(generate_gcode)
+print ("Generating GCODE from STL")
+generate_gcode = "prusa-slicer-console -g test.stl --load printer_profiles/" + profile_name + " -o gcode/test.gcode"
+os.system(generate_gcode)
 
 #print ("Cleaning Up")
 #os.system("del test.stl test.scad")
